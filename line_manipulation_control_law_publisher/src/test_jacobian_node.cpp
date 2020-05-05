38fa9dec2b092e15f91933ac2c69b1b58515bbcc
Danop Rajabhandharaks <drajabhandharaks@scu.edu>
2020-05-04 17:43:59 -0700

    Minor update

--- line_manipulation_control_law_publisher/src/test_jacobian_node.cpp
+++ line_manipulation_control_law_publisher/src/test_jacobian_node.cpp
@@ -16,29 +16,36 @@ int main(int argc, char **argv)
     robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
     
     robot_state::RobotStatePtr kinematic_state(new robot_state::RobotState(kinematic_model));
-    //kinematic_state->setToDefaultValues();
+    kinematic_state->setToDefaultValues();
     const robot_state::JointModelGroup* joint_model_group = kinematic_model->getJointModelGroup("arm");
     const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
 
     //Get Joint value
     std::vector<double> joint_values;
     kinematic_state->copyJointGroupPositions(joint_model_group,joint_values);
+    joint_values[0] = 0.1;
+    joint_values[1] = -0.1;
+    joint_values[2] = 0.1;
+    joint_values[3] = 0.1;
+    kinematic_state->setJointGroupPositions(joint_model_group,joint_values);
     for (std::size_t i = 0; i < joint_names.size(); ++i){
         ROS_INFO("Joint %s: %f", joint_names[i].c_str(), joint_values[i]);
     }
     
     //Joint Limits
-    //joint_values[0] = 5.57;
+    
     //kinematic_state->setJointGroupPositions(joint_model_group,joint_values);
     ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
-    kinematic_state->enforceBounds();
-    ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
+    //kinematic_state->enforceBounds();
+    //ROS_INFO_STREAM("Current state is " << (kinematic_state->satisfiesBounds() ? "valid" : "not valid"));
 
     //FW Kinematics
     //kinematic_state->setToRandomPositions(joint_model_group);
     const Eigen::Affine3d& end_effector_state = kinematic_state->getGlobalLinkTransform("end_effector_link");
     ROS_INFO_STREAM("Translation: \n" << end_effector_state.translation() << "\n");
-    ROS_INFO_STREAM("Rotation: \n" << end_effector_state.rotation() << "\n");
+    Eigen::Quaterniond quat(end_effector_state.linear());
+    Eigen::Matrix<double,4,1> quaternion(quat.w(), quat.x(), quat.y(), quat.z());
+    ROS_INFO_STREAM("Rotation: \n" << quaternion << "\n");
 
     std::cout << joint_model_group->getLinkModelNames().back() << std::endl;
     //Get Jacobian
