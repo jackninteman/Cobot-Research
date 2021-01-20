b9f2ae8e524a4d346cae96a300e2f6df20a737e9
danoponline <drajabhandharaks@scu.edu>
2021-01-19 16:38:15 -0800

    Add Matplotlib for plotting routine

---
+++ plot/plot.py
@@ -0,0 +1,27 @@
+import rosbag
+import matplotlib.pyplot as plt
+plt.get_backend()
+
+bag = rosbag.Bag('result2.bag')
+
+# Initialize some empty lists for storing the data
+tt = []
+x = []
+y = []
+z = []
+
+for topic,msg,t in bag.read_messages(topics=['/pose']):
+    tt.append(t.to_sec())
+    x.append(msg.position.x)
+    y.append(msg.position.y)
+    z.append(msg.position.z)
+bag.close()
+#print tt
+plt.figure()
+plt.plot(tt, x, label='x')
+plt.plot(tt, y, label='y')
+plt.plot(tt, z, label='z')
+plt.legend()
+plt.xlabel('time (seconds)')
+plt.ylabel('Position (meters)')
+plt.show()
